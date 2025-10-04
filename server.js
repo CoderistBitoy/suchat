36222f2e5686b4038f8085408ff5773619bd0217
Jhelo Caguioa <jhelouibanez@gmail.com>
2025-10-05 01:37:21 +0800

    Prepare for Render deploymet

--- server.js
+++ server.js
@@ -2,25 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
+// Load .env
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const path = require('path');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/messages', require('./routes/message'));
 
-// Connect MongoDB
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+.catch(err => console.log('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
